47221e67dc18a8fcd54d923614995f5a454816d4
Timur Baiguzhinov <tim.baiguzhinov@gmail.com>
2025-09-15 22:46:42 +0500

    Added mock for users endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="frontend", html=True), name="site")
+user_router = APIRouter(prefix="/frontend-api", tags=['users'])
+
+
+@user_router.get("/users/me")
+def get_user_info():
+    return {
+        "email": "example@example.com",
+        "isActive": True,
+        "profileId": "1",
+        "registeredAt": "2025-06-15T18:29:56+00:00",
+        "updatedAt": "2025-06-15T18:29:56+00:00",
+        "username": "user123",
+    }
+
+
+app.include_router(user_router)
 
 app.mount("/assets", StaticFiles(directory="frontend/assets"), name="assets")
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="site")
