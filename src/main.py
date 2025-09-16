5995b70d345d71a7d89381b0eb1897e6d66757ba
Timur Baiguzhinov <tim.baiguzhinov@gmail.com>
2025-09-16 11:44:06 +0500

    Added pydantic model

--- src/main.py
+++ src/main.py
@@ -1,14 +1,26 @@
+from datetime import datetime
+
 from fastapi import APIRouter, FastAPI
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 app = FastAPI(root_path="/frontend-api")
 
 user_router = APIRouter(prefix="", tags=['users'])
 
 
-@user_router.get("/users/me")
+class UserModel(BaseModel):
+    email: str
+    isActive: bool
+    profileId: int
+    registeredAt: datetime
+    updatedAt: datetime
+    username: str
+
+
+@user_router.get("/users/me", response_model=UserModel)
 def get_user_info():
-    return {
+    data = {
         "email": "example@example.com",
         "isActive": True,
         "profileId": "1",
@@ -16,10 +28,11 @@ def get_user_info():
         "updatedAt": "2025-06-15T18:29:56+00:00",
         "username": "user123",
     }
+    return data
 
 
 app.include_router(user_router)
 
-app.mount("/assets", StaticFiles(directory="frontend/assets"), name="assets")
-
 app.mount("/", StaticFiles(directory="frontend", html=True), name="site")
+
+app.mount("/assets", StaticFiles(directory="frontend/assets"), name="assets")
