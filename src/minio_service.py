c98d89496a021aa139f8dfe32720c9164948fe00
Timur Baiguzhinov <tim.baiguzhinov@gmail.com>
2025-09-23 01:35:49 +0500

    Added variables to minio service

--- src/minio_service.py
+++ src/minio_service.py
@@ -1,33 +1,35 @@
 import aioboto3
 from aiobotocore.config import AioConfig
 
+from env_settings import settings
+
 
 class MinioService:
     def __init__(self):
         self.config = AioConfig(
-            max_pool_connections=50,
-            connect_timeout=10,
-            read_timeout=30,
+            max_pool_connections=settings.mn.max_pool_connections,
+            connect_timeout=settings.mn.connection_timeout,
+            read_timeout=settings.mn.read_timeout,
         )
 
     async def create_s3_client(self):
         session = aioboto3.Session()
         async with session.client(
             's3',
-            endpoint_url='http://localhost:9000',
-            aws_access_key_id='minioadmin',
-            aws_secret_access_key='minioadmin',
+            endpoint_url=settings.mn.api_endpoint,
+            aws_access_key_id=settings.mn.login,
+            aws_secret_access_key=settings.mn.password.get_secret_value(),
             region_name='us-east-1',
             config=self.config,
         ) as s3_client:
             return s3_client
 
-    async def upload_file(self, file_content: bytes):
+    async def upload_file(self, file_content: bytes, file_name: str):
         client = await self.create_s3_client()
         async with client as s3_client:
             upload_params = {
-                'Bucket': 'my-public-bucket',
-                'Key': 'index3.html',
+                'Bucket': settings.mn.bucket,
+                'Key': file_name,
                 'Body': file_content,
                 'ContentType': 'text/html',
                 'ContentDisposition': 'inline',
