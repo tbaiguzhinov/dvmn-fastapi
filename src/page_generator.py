8eb8812fe29b9c6428d5a9a905a918202c3fd235
Timur Baiguzhinov <tim.baiguzhinov@gmail.com>
2025-09-23 01:35:10 +0500

    Replaced local save with minio save

--- src/page_generator.py
+++ src/page_generator.py
@@ -1,17 +1,14 @@
 import anyio
 from html_page_generator import AsyncPageGenerator
 
+from minio_service import MinioService
+
 
 async def generate_page(user_prompt: str):
     generator = AsyncPageGenerator()
+    minio_service = MinioService()
     with anyio.CancelScope(shield=True):
         async for chunk in generator(user_prompt):
             print(chunk, end="", flush=True)
             yield chunk
-
-        # with open(generator.html_page.title + '.html', 'w', encoding='utf-8') as f:
-            # f.write(generator.html_page.html_code)
-        with open('generated/index.html', 'w', encoding='utf-8') as f:
-            f.write(generator.html_page.html_code)
-
-    print('Файл успешно сохранён!')
+        await minio_service.upload_file(generator.html_page.html_code, file_name="index.html")
